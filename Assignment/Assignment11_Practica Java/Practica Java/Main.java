c9cf0f8509c446c6a69cc9d801a56f12b03c1fd3
Freddy Paez Bolaños <freddyjair37@gmail.com>
2019-10-17 20:25:40 -0500

    Py and Java

---
+++ Assignment/Assignment11_Practica Java/Practica Java/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+
+    public static void main(String[] arg) {
+        
+        int i = 5; 
+        
+        System.out.println(1 == 5 'Esto es un 5','Esto no es un 5') ;
+
+       }
+
+    }
+
+}
---
+++ Assignment/Assignment11_Practica Java/Practica Java/Persona.java
@@ -0,0 +1,33 @@
+
+
+public class Persona{
+
+    private int num; 
+    private String str;
+
+    public Persona(){
+        
+    }
+
+    public Persona(int num){
+        this.num = num; 
+
+    }
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    
+}
