389bfeef60ad417548e23e1b9ea52230c41df8c8
Freddy Paez Bolaños <freddyjair37@gmail.com>
2019-10-20 22:13:24 -0500

    Java V1.0

---
+++ Assignment/Assignment11_Java ced/ClassCedula.java
@@ -0,0 +1,20 @@
+public class ClassCedula { 
+    public int cedula; 
+
+
+public ClassCedula(){
+
+}
+
+public ClassCedula(int cedula){
+    this.cedula = cedula;
+}
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+}
