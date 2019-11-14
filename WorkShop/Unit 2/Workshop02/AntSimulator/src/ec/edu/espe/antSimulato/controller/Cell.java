79158490879a398f188234a94c1dcb8c4f442021
Freddy Paez Bolaños <freddyjair37@gmail.com>
2019-11-13 19:39:59 -0600

    Unit2!

---
+++ WorkShop/Unit 2/Workshop02/AntSimulator/src/ec/edu/espe/antSimulato/controller/Cell.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.antSimulato.controller;
+
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Labs-DCCO
+ */
+public class Cell {
+
+    private int row;
+    private int col;
+
+    public void receivePheromone() {
+
+    }
+
+    public void getAnts() {
+        Arraylist<Ant> antList = new ArrayList<>;
+    }
+
+}
